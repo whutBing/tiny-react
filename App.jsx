874cc33b0fb2063842691b31341d54a7a0ad50d2
Bing <zhengyabing2012@163.com>
2024-01-16 12:26:15 +0800

    feat: class2-jsx

---
+++ App.jsx
@@ -0,0 +1,8 @@
+import React from "./vite-runner/core/React.js";
+// const App = React.createElement("div", { id: "app" }, "hi", "mini-react");
+function AppOne() {
+  return <div>App.jsx</div>;
+}
+const App = <div>App.jsx1</div>;
+console.log("=====App.jsx======", AppOne);
+export default App;
