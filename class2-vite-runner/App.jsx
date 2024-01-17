4c1e8b344b0c8b78809dae1c959be7a9395bfadc
Bing <zhengyabing2012@163.com>
2024-01-17 15:32:51 +0800

    feat: class4-实现 fiber结构

--- class2-vite-runner/App.jsx
+++ class2-vite-runner/App.jsx
@@ -1,3 +1,4 @@
 import React from "./core/React.js";
 const App = React.createElement("div", { id: "app" }, "hi", "mini-react");
+console.log('=====App.jsx=====', App)
 export default App;
--- class2-vite-runner/core/React.js
+++ class2-vite-runner/core/React.js
@@ -8,6 +8,7 @@ function createTextNode(text) {
     },
   };
 }
+
 function createElement(type, props, ...children) {
   return {
     type,
@@ -21,27 +22,85 @@ function createElement(type, props, ...children) {
 }
 
 function render(el, container) {
-  const dom =
-    el.type === "TEXT_ELEMENT"
-      ? document.createTextNode("")
-      : document.createElement(el.type);
-
-  Object.keys(el.props).forEach((key) => {
-    if (key !== "children") {
-      dom[key] = el.props[key];
+  debugger;
+  nextWorkUnit = {
+    dom: container,
+    props: {
+      children: [el]
     }
-  });
-
-  const children = el.props.children;
-  children.forEach((child) => {
-    render(child, dom);
-  });
-  container.append(dom);
+  }
+  
+  //
+  // const children = el.props.children;
+  // children.forEach((child) => {
+  //   render(child, dom);
+  // });
+  // container.append(dom);
 }
+
+
+
+let nextWorkUnit = null;
+
+function performWorkOfUnit(work) {
+  debugger;
+  if (!work.dom) {
+    // 1. 创建 dom
+    const dom = (work.dom =
+      work.type === "TEXT_ELEMENT"
+        ? document.createTextNode("")
+        : document.createElement(work.type));
+    work.parent.dom.append(dom)
+    // 2. 处理 props
+    Object.keys(work.props).forEach((key) => {
+      if (key !== "children") {
+        dom[key] = work.props[key];
+      }
+    });
+  }
+  // 3. 转换链表 设置好指针
+  const children = work.props.children;
+  let prevChild = null;
+  children.forEach((child, index) => {
+    const newWork = {
+      type: child.type,
+      props: child.props,
+      child: null,
+      parent: work,
+      sibling: null,
+      dom: null
+    }
+    debugger
+    if (index === 0) {
+      work.child = newWork;
+    } else {
+      prevChild.sibling = newWork;
+    }
+    prevChild = newWork;
+  })
+  // 4. 返回下一个要执行的任务
+  if (work.child) {
+    return work.child
+  }
+  if (work.sibling) {
+    return work.sibling;
+  }
+  return work.parent?.sibling
+}
+
+function workLoop(deadline) {
+  let shouldYield = false;
+  while (!shouldYield && nextWorkUnit) {
+    nextWorkUnit = performWorkOfUnit(nextWorkUnit);
+    shouldYield = deadline.timeRemaining() < 1
+  }
+  requestIdleCallback(workLoop)
+}
+
+requestIdleCallback(workLoop);
 
 const React = {
   render,
   createElement,
 };
-
 export default React;
